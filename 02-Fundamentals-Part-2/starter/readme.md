836fd294a00130867901486975aa746c2f58912e
Nichele Dunn <the7yearitch@hotmail.com>
2021-11-09 11:51:30 -0700

    ✨Section 32 Activating Strict Mode / section 33 Functions

---
+++ 02-Fundamentals-Part-2/starter/readme.md
@@ -0,0 +1,24 @@
+# Complete JavaScript - Section 3: JavaScript Fundamentals Part 2
+
+## Activating Strict Mode
+* Secure mode can be activated
+* A special mode that makes it easier to write secure JavaScript code
+* To activate, add the string "use strict"; at the beginning of the script
+  * If using, must be the first line of any script
+  * Can also be activated only for a specific function or block
+* Makes it easier to avoid accidental errors
+  * Forbids certain actions
+  * Creates visible errors in certain situations
+* I'm curious if TypeScript has any similarity to using Strict Mode
+* Also introduces a list of variables names that are reserved for future use
+* The remainder of the course assumes that Strict Mode is being utilized
+
+## Functions
+* A piece of code that can be reused over and over again in a script
+  * Can hold one or more complete lines of code
+  * Curly braces `{}` hold the function body
+* Functions often take in data, and can also return data back to the script
+  * Think of functions as machines for the code
+  * Food processor analogy (food processor = function body)
+* Parameters are defined when a function is invoked (arguments passed through to the function);
+* Set function invocations to a variable to utilize their returned values in the script
---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,51 @@
+"use strict";
+console.log("👩‍🏫 Section 3 - JavaScript Fundamentals Part 2");
+
+console.log("👩‍🏫 Activating Strict Mode");
+// statement on first line activates Strict Mode (SM)
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true; // in SM, omit part of the name to force an error; without SM, this line creates a new variable and assignes its value
+if (hasDriversLicense) console.log("I can drive 🏎");
+
+// const interface = "Audio"; // interface is a reserved identifier; can't use this variable name when using SM
+// const private = 777; // private is also a reserved identifier
+
+console.log("👩‍🏫 Functions");
+function logger() {
+  console.log("My name is Nichele 🛼");
+}
+
+// invoke the function, run the function, call the function
+logger();
+
+// function creates juice from the fruits passed through
+function fruitProcessor(numApples, numOranges) {
+  const juice = `Your juice was made with ${numApples} apples and ${numOranges} oranges.`;
+  return juice; 
+}
+
+fruitProcessor(2, 3); // store this value in a variable to use the returned value of `juice` in the script
+const morningBev = fruitProcessor(1, 3);
+console.log("🌞🧃", morningBev);
+const orangeOnly = fruitProcessor(0, 3);
+console.log("🍊🍊🍊!", orangeOnly);
+
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
+// console.log("👩‍🏫 ");
