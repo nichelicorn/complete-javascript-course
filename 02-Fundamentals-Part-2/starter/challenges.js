2216fdf20bce8fc2380c4e1fc9706bd120e21125
Nichele Dunn <the7yearitch@hotmail.com>
2022-01-21 10:51:05 -0700

    ✨ Coding Challenge Part 2 / no. 2

--- 02-Fundamentals-Part-2/starter/challenges.js
+++ 02-Fundamentals-Part-2/starter/challenges.js
@@ -62,3 +62,81 @@ function checkWinner(dolphAvg, koalasAvg) {
 
 console.log("Match one results: ", checkWinner(dolphsAvg1, koalasAvg1));
 console.log("Match two results: ", checkWinner(dolphsAvg2, koalasAvg2));
+
+console.log("Coding Challenge #2 👩‍🏫");
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+// Your tasks:
+// ✅ 1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+// ✅ 2. And now let's use arrays! So create an array 'bills' containing the test data below
+// ✅ 3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+
+// Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+// GOOD LUCK 😀
+
+// Test data
+const bills = [ 125, 555, 44 ];
+const tips = [];
+const totals = [
+  // {
+  //   bill: 0,
+  //   tip: 0,
+  //   total: 0
+  // } // the desired shape of the objects to be stored in this array
+]
+
+// Create function
+// function calculateTips(bill) {
+//   let tip = 0;
+
+//   if (bill <= 300 && bill >= 50) {
+//     return tip = bill * .15;
+//   } else {
+//     return tip = bill * .20;
+//   }
+// }
+
+// Refactor function to work on an array of bills
+function calculateTips(array) {
+  let tip = 0;
+
+  return array.map(bill => {
+    if (bill <= 300 && bill >= 50) {
+      tips.push((bill * .15));
+      return tip = bill * .15;
+    } else {
+      tips.push(bill * .20);
+      return tip = bill * .20;
+    }
+  })
+}
+
+// console.log("tip for a bill of $100 >", calculateTips(100));
+// console.log("tip for a bill of $15 >", calculateTips(1500));
+
+calculateTips(bills);
+console.log("the tips for an array of bills >", tips);
+
+function calculateBillTotals(array) {
+  // map through the array of numbers and create new objects to store several values
+  return array.map(bill => {
+    if (bill >= 50 && bill <= 300) {
+      totals.push({
+        bill: bill,
+        tip: (bill * .15),
+        total: bill + (bill * .15)
+      })
+    } else {
+      totals.push({
+        bill: bill,
+        tip: (bill * .20),
+        total: bill + (bill * .20)
+      })
+    }
+  })
+}
+
+calculateBillTotals(bills);
+console.log("the total of bills, itemized >", totals);
