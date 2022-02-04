ba8c0eccf5e016300f82749c5259edd2de01d9fe
Nichele Dunn <the7yearitch@hotmail.com>
2022-02-04 11:21:20 -0700

    🧪 Bonus coding challenge

--- 02-Fundamentals-Part-2/starter/challenges.js
+++ 02-Fundamentals-Part-2/starter/challenges.js
@@ -238,6 +238,33 @@ console.log("calculated totals >", totals4);
 
 // Bonus:
 // 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a difficult challenge (we haven't done this before)! Here is how to solve it:
-// 4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
-// 4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
-// 4.3. Call the function with the 'totals' array
+
+function calcAverage(array) {
+  // calculate the average of all numbers in the array
+  let grandTotal = 0;
+  let average = 0;
+  const divisor = array.length;
+
+  for (let i = 0; i < array.length; i++) {
+    grandTotal+=array[i];
+    if (!grandTotal) break;
+    console.log(`grandTotal is ${grandTotal}`);
+    average = grandTotal / divisor;
+  }
+
+  console.log(`grandTotal: ${grandTotal} and the divisor: ${divisor};`)
+
+  // average = grandTotal / divisor;
+  console.log(`The average of these numbers is ${average}.`);
+  return average;
+
+}
+
+calcAverage(bills4);
+calcAverage(totals4);
+
+
+
+// √ 4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. 
+// √ Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+// √ 4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
