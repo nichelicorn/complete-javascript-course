00377a2ff7267f5d3adab82d481dcc8dfeee2ce4
Nichele Dunn <the7yearitch@hotmail.com>
2022-02-28 13:20:04 -0700

    ♻️  Reorganize selectors by alpha and update comments, delete console.logs

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -5,18 +5,17 @@ let currentScore = 0;
 let currentPlayer;
 
 // 🔎 Selected elements
-const score0El = document.getElementById("score--0");
-const score1El = document.getElementById("score--1");
-const diceEl = document.querySelector(".dice");
-// const btns = document.querySelectorAll(".btn");
+const btnHold = document.querySelector(".btn--hold");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
 
 // 🎬 Starting conditions
-// set scores to zero and hide dice on page load
+// set scores to zero and hide dice on page load; set current player
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
@@ -34,27 +33,22 @@ function rollDice() {
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${roll}.png`
   
-  // add roll to player score
-  
   // check if the number rolled === 1
   console.log("roll >", roll);
   if (roll !== 1) {
-    // add roll to current score
-    // display in player's current score box
+    // update and display current score
     currentScore += roll;
-    // console.log("current score >", currentScore);
-    // refactor to display score in the current player's board
     current0El.textContent = currentScore;
   } else {
-    console.log("player rolled a 1!");
-    console.log("current player >", currentPlayer);
-    // if (currentPlayer === "name--0") {
-    //   currentPlayer = "name--1";
-    // } else {
-    //   currentPlayer = "name--0";
-    // }
+    // if number rolled === 1: set player score to zero; switch to next player
+    currentScore = 0;
+    current0El.textContent = currentScore;
+    // console.log("player rolled a 1!");
+    // console.log("current player >", currentPlayer);
     currentPlayer = (currentPlayer === "name--0") ? "name--1" : "name--0";
     console.log("reset current player?? >", currentPlayer);
-    // if number rolled === 1: set player score to zero; switch to next player
   }
+
+    // add roll to current player score
+  
 };
