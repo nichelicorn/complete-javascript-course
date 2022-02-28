03ba1529653a63969f14ecf1201eca272876b5da
Nichele Dunn <the7yearitch@hotmail.com>
2022-02-28 13:50:31 -0700

    ✨ Work on condition to switch active player display

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,7 +2,7 @@
 
 // 💾 Application state
 let currentScore = 0;
-let currentPlayer, currentScoreDisplay;
+let currentPlayer, currentScoreDisplay, currentActive;
 
 // 🔎 Selected elements
 const btnHold = document.querySelector(".btn--hold");
@@ -11,6 +11,8 @@ const btnRoll = document.querySelector(".btn--roll");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
 const diceEl = document.querySelector(".dice");
+const player0ActiveEl = document.querySelector(".player--0");
+const player1ActiveEl = document.querySelector(".player--1");
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
 
@@ -19,6 +21,7 @@ const score1El = document.getElementById("score--1");
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
+currentActive = player0ActiveEl;
 currentPlayer = "name--0";
 currentScoreDisplay = current0El;
 
@@ -40,22 +43,34 @@ function rollDice() {
     currentScore += roll;
     currentScoreDisplay.textContent = currentScore;
   } else {
-    // if number rolled === 1: set current player score to zero; switch to next player
+    // if number rolled === 1: set current player score to zero; switch to next player; update visual cue of current player
     console.log("🧟‍♀️...player rolled a 1!");
+
     // set current player score to zero and display;
     currentScore = 0;
     currentScoreDisplay.textContent = currentScore;
-    console.log("currentScore >", currentScore);
-    // current0El.textContent = currentScore;
-    // console.log("current player >", currentPlayer);
+
+    // reset current player to the next player
     currentPlayer = (currentPlayer === "name--0") ? "name--1" : "name--0";
-    console.log("reset current player >", currentPlayer);
+    // console.log("reset current player >", currentPlayer);
+
+    // update visual cue of current player
+    console.log("currentActive >", currentActive);
+    if (currentActive === player0ActiveEl) {
+      player1ActiveEl.classList.add("player--active");
+      player0ActiveEl.classList.remove("player--active");  
+    } else {
+      player0ActiveEl.classList.add("player--active");  
+      player1ActiveEl.classList.remove("player--active");
+    }
+    // currentActive = (currentActive === player0ActiveEl) ? player1ActiveEl.classList.add("player--active") : player0ActiveEl.classList.add("player--active");
+
     // reset currentScoreDisplay
     currentScoreDisplay = (currentScoreDisplay === current0El) ? current1El : current0El;
   }
 
   // console.log("currentScore >", currentScore);
-  console.log("currentPlayer >", currentPlayer);
+  // console.log("currentPlayer >", currentPlayer);
   // console.log("currentScoreDisplay >", currentScoreDisplay);
 
   // add roll to current player score
