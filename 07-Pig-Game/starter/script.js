d96c060ee9f6ffe72603662e99ac8e9f9e01c4a7
Nichele Dunn <the7yearitch@hotmail.com>
2022-02-28 12:58:13 -0700

    ✨ Add currentScore to application state

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// 💾 Application state
+let currentScore = 0;
+
 // 🔎 Selected elements
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
@@ -9,24 +12,36 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+// 🎬 Starting conditions
 // set scores to zero and hide dice on page load
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+// 🎧 Event listeners
 btnRoll.addEventListener("click", rollDice);
 
 // Function to roll the dice
-// check if the number rolled === 1
 // if number rolled === 1: set player score to zero; switch to next player
 function rollDice() {
   // √ generate a random dice roll
   const roll = Math.trunc(Math.random() * 6) + 1;
- 
-   
-  // display the dice corresponding to the roll
+  
+  // √ display the dice corresponding to the roll
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${roll}.png`
+  
+  // add roll to player score
 
+  // check if the number rolled === 1
   console.log("roll >", roll);
-}
+  if (roll !== 1) {
+    // add roll to current score
+    currentScore += roll;
+    console.log("current score >", currentScore);
+  } else {
+    // if roll is 1
+    console.log("player rolled a 1!");
+    //switch to nex player
+  }
+};
