4000ccaf4f9ba2418ce14d6753b1183a648bce77
Nichele Dunn <the7yearitch@hotmail.com>
2022-03-04 12:07:49 -0700

    ♻️  Update active class when game starts

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -69,11 +69,34 @@ function switchPlayer() {
    currentScoreDisplay = (currentScoreDisplay === current0El) ? current1El : current0El;
 
    // set active player status on game board
-  //  🐛 this is where the bug is -- it's just toggling these modes, not verifying the active player and displaying that
+// fix active player display error -- if player 2 wins the game, the white overlay doesn't apply to the correct player; scores / state still updating correctly
+   //  🐛 this is where the bug is -- it's just toggling these modes, not verifying the active player and displaying that
   // go back to using the currentActive? seemed like it worked okay when that was being used
   // hopefully this is in the git history
-   player0ActiveEl.classList.toggle("player--active");
-   player1ActiveEl.classList.toggle("player--active");
+
+  console.log("new current player >", currentPlayer + 1);
+
+  if (currentPlayer === 0) {
+    player0ActiveEl.classList.add("player--active");
+    player1ActiveEl.classList.remove("player--active");
+
+  } else {
+    player0ActiveEl.classList.remove("player--active");
+    player1ActiveEl.classList.add("player--active");
+
+  }
+
+   
+  //  console.log("checking currentActive >", currentActive);
+
+  // if (currentActive.classList.contains("player--0")) {
+  //   console.log("current player is now player 2");
+  //   currentActive = player0ActiveEl;
+  // } else {
+  //   currentActive = player1ActiveEl;
+  //   console.log("current player is now player 1");
+  // }
+
 };
 
 function holdScore() {
@@ -104,6 +127,8 @@ function startGame(event) {
   currentScoreDisplay = current0El;
   current0El.textContent = 0;
   current1El.textContent = 0;
+  player0ActiveEl.classList.add("player--active");
+    player1ActiveEl.classList.remove("player--active");
   player0ActiveEl.classList.remove("player--winner");    player1ActiveEl.classList.remove("player--winner")
   btnRoll.disabled = false;
   btnHold.disabled = false;;
